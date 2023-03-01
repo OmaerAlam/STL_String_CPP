e0dc89e8d399b7bb8b4c3c218ba6af90c1286b9a
OmaerAlam <114277920+OmaerAlam@users.noreply.github.com>
2023-03-01 18:11:02 +0600

    B. String Initialization.

---
+++ String_2.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s="CoderOmaer";// 1 No. Process.
+    cout<<s<<endl;
+
+
+    cout<<endl;
+
+
+    string st="coder";// 2 No. Process.
+    st+=' ';
+    st+="omaer";
+    cout<<st<<endl;
+
+return 0;
+}
