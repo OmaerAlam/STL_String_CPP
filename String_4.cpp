2677b3b2fe5356af1259c85be55c53f6dc3eb94a
OmaerAlam <114277920+OmaerAlam@users.noreply.github.com>
2023-03-01 18:45:40 +0600

    D. String Print from index value.

---
+++ String_4.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   string s="CoderOmaer";
+  cout<<s[0]<<" "<<s[5]<<" "<<s[9];
+
+  cout<<endl;
+
+  s[0]='M', s[5]='L',s[9]='D';
+  cout<<s[0]<<" "<<s[5]<<" "<<s[9]<<endl;
+return 0;
+}
