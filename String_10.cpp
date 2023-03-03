e6e3944eaf5d3850c9f530e239722b840f90bcbe
OmaerAlam <114277920+OmaerAlam@users.noreply.github.com>
2023-03-03 19:59:43 +0600

    J. String Iterator.

---
+++ String_10.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   string s="Coder Omaer";
+   string::iterator it;
+   for(it=s.begin(); it!=s.end();it++){
+    cout<<*it;
+   }
+   cout<<endl;
+return 0;
+}
