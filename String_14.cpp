9cd3dd217e5563f467b05f8ca0e5439c42aed6df
OmaerAlam <114277920+OmaerAlam@users.noreply.github.com>
2023-03-03 20:46:41 +0600

    Update String_14.cpp

--- String_14.cpp
+++ String_14.cpp
@@ -1,12 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    //1 No process
    string s="aabaa";
    string tmp=s;
    reverse(tmp.begin(),tmp.end());
    if(s==tmp) cout<<"palindrome\n";  //Time Complexity- O(n).
    else cout<<"Not palindrome\n";    //Time Complexity- O(n).
    cout<<s<<endl;
+
+   cout<<endl;
+
+//2 No process
+   string s1="aabAa";
+   string tmp1=s1;
+   reverse(tmp1.begin(),tmp1.end());
+   if(s1==tmp1) cout<<"palindrome\n";  //Time Complexity- O(n).
+   else cout<<"Not palindrome\n";    //Time Complexity- O(n).
+   cout<<s1<<endl;
 
 return 0;
 }
