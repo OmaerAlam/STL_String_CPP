08a696ff6e1a84e0d678f56efda1b585eeecdea2
OmaerAlam <114277920+OmaerAlam@users.noreply.github.com>
2023-03-03 20:10:03 +0600

    L. String- Two String Comparison.

---
+++ String_12.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //1 NO process
+   string s1="Programmer",s2="Coder";
+   if(s1==s2) cout<<"Equal"<<endl;
+   else cout<<"Not Equal"<<endl;
+
+   cout<<endl;
+
+   //2 No process
+   string s3="Coder", s4="Coder";
+   if(s3==s4) cout<<"Equal"<<endl;
+   else cout<<"Not Equal"<<endl;
+return 0;
+}
