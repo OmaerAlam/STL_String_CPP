b2d4e09913f1f784237e87df8b689800cfd0ffdb
OmaerAlam <114277920+OmaerAlam@users.noreply.github.com>
2023-03-03 19:15:22 +0600

    E. String Concatenation with string.

---
+++ String_5.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //1 No process-
+   string s,s1="Coder", s2="Omaer";
+   char c=' ';
+   s=s1+c+s2;
+   cout<<s<<endl;
+
+  cout<<endl;
+
+  //2 No process-
+  string s4="Programmer",s5="Omaer";
+  char c1=' ';
+  s4+=c1;
+  s4 += s5;
+  cout<<s4<<endl;
+
+  cout<<endl;
+
+  //3 NO process-
+  string s6;
+  s6 +="Omaer";
+  s6+= ' ';
+  s6+="Alam";
+  cout<<s6<<endl;
+return 0;
+}
